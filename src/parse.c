5cae0dfcb8dadfed79b18e204283ca6e388f6207
Rubén Díaz Fraga <rubendfraga@gmail.com>
2023-12-14 00:38:03 +0100

    Removed not used files

--- src/parse.c
+++ src/parse.c
@@ -12,22 +12,4 @@
 
 #include "push_swap.h"
 
-// Function to parse arguments and create stack
-int parse_args(int argc, char **argv, t_stack **a)
-{
-    // Parsing logic here
-    return 1; // Return 1 if successful, 0 if there is an error
-}
 
-// Function to display error message and exit
-void error_exit(void)
-{
-    write(2, "Error\n", 6);
-    exit(EXIT_FAILURE);
-}
-
-// Function to free stacks
-void free_stacks(t_stack **a, t_stack **b)
-{
-    // Logic to free both stacks
-}
