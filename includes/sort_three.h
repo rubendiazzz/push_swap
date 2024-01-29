22f649ce87c5cf9ace82dae674dceb7b67153f4b
Ruben Diaz <rdiaz-fr@student.42malaga.com>
2024-01-29 16:15:54 +0100

    Add sort_three.h header file

---
+++ includes/sort_three.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_three.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdiaz-fr <rdiaz-fr@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 15:56:04 by rdiaz-fr          #+#    #+#             */
+/*   Updated: 2024/01/29 15:56:07 by rdiaz-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SORT_THREE_H
+# define SORT_THREE_H
+
+# include "stack_operations.h"
+
+void	sort_three(t_node **stack);
+
+#endif
