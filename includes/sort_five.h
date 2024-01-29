a835a73e6ec68b65188641042347cab4fab92640
Ruben Diaz <rdiaz-fr@student.42malaga.com>
2024-01-29 16:15:43 +0100

    Add sort_five.h header file

---
+++ includes/sort_five.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_five.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdiaz-fr <rdiaz-fr@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 15:57:32 by rdiaz-fr          #+#    #+#             */
+/*   Updated: 2024/01/29 15:57:48 by rdiaz-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SORT_FIVE_H
+# define SORT_FIVE_H
+
+# include "sort_three.h"
+# include "stack_operations.h"
+
+void	sort_five(t_node **stack_a, t_node **stack_b);
+
+#endif
