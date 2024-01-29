cdc81823d8da45459984791678b47a179e8b1c98
Ruben Diaz <rdiaz-fr@student.42malaga.com>
2024-01-29 16:15:51 +0100

    Add sort_large.h header file

---
+++ includes/sort_large.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_large.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdiaz-fr <rdiaz-fr@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 16:07:07 by rdiaz-fr          #+#    #+#             */
+/*   Updated: 2024/01/29 16:07:32 by rdiaz-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SORT_LARGE_H
+# define SORT_LARGE_H
+
+# include "../includes/stack_operations.h"
+
+void	sort_large(t_node **stack_a, t_node **stack_b);
+
+#endif
